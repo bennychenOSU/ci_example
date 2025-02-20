117aa37a2122e14710cdbc03171b1a6a42621e0b
bennychenOSU <160067513+bennychenOSU@users.noreply.github.com>
2025-02-20 23:58:35 +0000

    add test2

--- tests.py
+++ tests.py
@@ -1,4 +1,17 @@
 import unittest
+import task
 
-if __name__ == '__main__':
+
+class TestCase(unittest.TestCase):
+
+    def test1(self):
+        expected = "Hello World"
+        self.assertEqual(task.my_func(), expected)
+      
+    def test2(self):
+        expected = "Hola World"
+        self.assertEqual(task.my_func(), expected)
+
+
+  if __name__ == '__main__':
     unittest.main()
